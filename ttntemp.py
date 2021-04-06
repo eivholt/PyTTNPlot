8d00168d35a673293431b64caee9d18e6a87f907
Eivind Holt <eivholt@gmail.com>
2021-04-06 08:47:37 +0200

    Added cli

--- ttntemp.py
+++ ttntemp.py
@@ -1,10 +1,11 @@
 # %%
 import sys
+import datetime
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import json
 import pandas as pd
-import datetime
 import requests
 import secrets
 
@@ -12,30 +13,35 @@ from requests.exceptions import HTTPError
 
 print(f"Name of the script      : {sys.argv[0]}")
 print(f"Arguments of the script : {sys.argv[1:]}")
+argparser = argparse.ArgumentParser(description="Graph temperature measurements.")
+argparser.add_argument("--file", nargs='?', type=str, help="path to file containing json list of measurements")
+
+args = argparser.parse_args()
+inputPath = args.file
+
+if inputPath is not None:
+    temperatureDataFrame = pd.read_json(inputPath)
+else:
+    # Call api
+    try:
+        secrets = secrets.Secrets()
+        response = "asdf"
+        response = requests.get(secrets.dataApiUrl,
+            headers={'Accept': 'application/json', 'Authorization': secrets.authorizationKey},)
+        response.raise_for_status()
+        temperaturesAsJson = response.json()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+        exit()
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+        exit()
+    else:
+        print('Success!')
+        temperatureDataFrame = pd.DataFrame.from_dict(temperaturesAsJson)
 
 # Initialise a figure. subplots() with no args gives one plot.
 fig, axs = plt.subplots(sharex="all", sharey="all")
-
-# Call api
-try:
-    secrets = secrets.Secrets()
-    response = requests.get(secrets.dataApiUrl,
-        headers={'Accept': 'application/json', 'Authorization': secrets.authorizationKey},)
-    response.raise_for_status()
-    temperaturesAsJson = response.json()
-except HTTPError as http_err:
-    print(f'HTTP error occurred: {http_err}')
-    exit()
-except Exception as err:
-    print(f'Other error occurred: {err}')
-    exit()
-else:
-    print('Success!')
-    temperatureDataFrame = pd.DataFrame.from_dict(temperaturesAsJson) #pd.read_json(temperaturesAsJson)
-
-#with open('ttn.json') as jsonFile:
-#    deviceDataDict = json.load(jsonFile) 
-#temperatureDataFrame = pd.DataFrame(deviceDataDict)
 
 temperatureDf = temperatureDataFrame[temperatureDataFrame.tempc1 != -127][temperatureDataFrame.tempc2 != -127]
 temperatureDf["datetime"] = pd.to_datetime(temperatureDf["time"])
